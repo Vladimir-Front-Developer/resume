b18bb86196b84d6180c1814bd734b1a10baa689d
Vladimir-Front-Developer <wladimirtltlt@yandex.ru>
2019-02-05 19:02:15 +0400

    add menuBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent
   ],
   imports: [
+    MatMenuModule,
+    MatButtonModule,
+    FormsModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
